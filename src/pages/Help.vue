aa5bdc6aacfe7dea318945e7a2c310df58687b61
MikyPopescu <popescu9andreea17@stud.ase.ro>
2020-03-15 10:26:14 +0200

    Basic todo app

---
+++ src/pages/Help.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias esse laboriosam eius consectetur fuga illo earum, dicta aut possimus rerum saepe, non culpa, aspernatur ea? Dolore illo vitae sit hic.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptas quis, rem nostrum vel distinctio numquam veniam molestias recusandae quae dignissimos possimus. Aperiam consequatur doloremque atque possimus laborum harum unde!</p>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white" separator bordered>
+      <q-item v-for="task in tasks" :key="task.title" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{task.title}}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: "Get the trash",
+          done: false
+        },
+        {
+          title: "Eat bananas",
+          done: false
+        },
+        {
+          title: "Go shopping",
+          done: false
+        }
+      ]
+    };
+  }
+};
+</script>
